f7dcfd6d11bbf42086fb80f60b08f4d53b6d153e
Ana Ventura <anaventura1811@gmail.com>
2024-10-08 15:03:03 -0300

    feat: create user and project models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()
+            ->count(200)
+            ->create();
+
+        User::query()->inRandomOrder()->limit(10)->get()->each(fn(User $user) => 
+            Project::factory()
+                ->count(10)
+                ->create(['created_by' => $user->id])
+        );
+        
     }
 }
