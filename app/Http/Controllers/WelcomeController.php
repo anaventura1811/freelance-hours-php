e933067c66b4162a648710c3791f345f65927f27
Ana Ventura <anaventura1811@gmail.com>
2024-10-08 12:47:54 -0300

    understanding schema of folders, as well as controllers, creation of fields and updates

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
@@ -11,6 +12,25 @@ class WelcomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-       return view('welcome');
+
+        $user = User::find(1);
+
+        // $user->update(['email_verified_at' => now()->subMonths(10)]);
+        // $user = User::query()->create([
+        //     'name' => 'Ana',
+        //     'email' => 'anaventura@gmail.com',
+        //     'password' => 'teste1234'
+        // ]);
+
+        // $user->email_verified_at = now();
+        // $user->save();
+
+        $user->update(['email_verified_at' => now()->subMonths(10)]);
+
+        dd($user->email_verified_at);
+
+        // now()->date;
+
+       return view('test.ana');
     }
 }
