f67fbea4d2573b78668965aa9468f03b7a6d9802
Ana Ventura <anaventura1811@gmail.com>
2024-10-08 16:58:44 -0300

    feat: apply clean code

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,9 +1,9 @@
 <div>
 
     <x-layouts.app>
-        <livewire:projects.show  :project="$project" />
-        
-        <livewire:projects.proposals :project="$project" />
+        <livewire:projects.show  :$project />
+
+        <livewire:projects.proposals :$project />
     </x-layouts.app>
 
 </div>
