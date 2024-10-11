ff97e35f29a838845070d7203a672154a4680537
Ana Ventura <anaventura1811@gmail.com>
2024-10-11 13:19:12 -0300

    feat: add ranking to proposals position

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Actions\ArrangePositions;
 use App\Models\Project;
 use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,11 +22,13 @@ class DatabaseSeeder extends Seeder
             ->count(200)
             ->create();
 
-        User::query()->inRandomOrder()->limit(10)->get()->each(function (User $user) {
-                $project = Project::factory()
-                    // ->count(10)
-                    ->create(['created_by' => $user->id]);
+        User::query()->inRandomOrder()->limit(10)->get()
+        ->each(function (User $user) {
+                $project = Project::factory()->create(['created_by' => $user->id]);
+
                 Proposal::factory()->count(random_int(4,45))->create(['project_id' => $project->id]);
+
+                ArrangePositions::run($project->id);
             
             }
         );
